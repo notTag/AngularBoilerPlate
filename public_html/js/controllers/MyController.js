a3e467ec0359a3684f5868721b7b8602f834bf49
Nick Tag <nick.tagliasacchi@gmail.com>
2017-04-17 20:49:19 -0400

    Adding a simple directive

--- public_html/js/controllers/MyController.js
+++ public_html/js/controllers/MyController.js
@@ -1,3 +1,8 @@
 angular.module("app").controller("MyCtlr", function($scope){
     $scope.title = "My View";
+})
+.directive('myDirective', function() {
+  return {
+    template: 'My Directive Text'
+  };
 });
